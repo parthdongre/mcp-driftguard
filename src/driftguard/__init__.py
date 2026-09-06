19afef4ad9d411de95e2ce1f56f22a4cebdb3226
Parthozy <163404890+parthdongre@users.noreply.github.com>
2026-09-06 11:11:53 +0530

    Expose learned pair-classifier research API

--- src/driftguard/__init__.py
+++ src/driftguard/__init__.py
@@ -3,11 +3,13 @@
 from .canonicalize import make_snapshot
 from .diff import build_delta
 from .features import extract_pair_features
+from .learning import LogisticPairClassifier, record_features, repository_group_split
 from .models import ChangeClass, ToolDelta, ToolSnapshot
 from .temporal import SequentialDriftMonitor, TemporalConfig
 
 __all__ = [
     "ChangeClass",
+    "LogisticPairClassifier",
     "SequentialDriftMonitor",
     "TemporalConfig",
     "ToolDelta",
@@ -15,5 +17,7 @@ __all__ = [
     "build_delta",
     "extract_pair_features",
     "make_snapshot",
+    "record_features",
+    "repository_group_split",
 ]
-__version__ = "0.2.0"
+__version__ = "0.3.0"
